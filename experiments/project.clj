20f5e946612400521b0aa5acd5f1a253fc2165ac
Andy Smith <andyrsmith@googlemail.com>
2012-05-10 14:57:38 +0800

    incremental work - clojure csv parser

---
+++ experiments/project.clj
@@ -0,0 +1,4 @@
+(defproject experiments "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :plugins [[lein-swank "1.4.4"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ experiments/test/experiments/test/core.clj
@@ -0,0 +1,6 @@
+(ns experiments.test.core
+  (:use [experiments.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
