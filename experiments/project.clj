f5c3f830cfc9a74894e2783ae75319a279330acb
Andy Smith <andyrsmith@googlemail.com>
2013-07-03 12:59:38 +0100

    Duh2

---
+++ experiments/project.clj
@@ -0,0 +1,6 @@
+(defproject experiments "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [
+                 [org.clojure/clojure "1.5.1"]
+                 [quil "1.6.0"]
+                 ])
