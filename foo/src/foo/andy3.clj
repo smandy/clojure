168a2a9ca60fd5dc93da63c0076698f2a5959d8c
Andy Smith <andyrsmith@googlemail.com>
2014-10-29 23:12:15 +0000

    Incremental

--- foo/src/foo/andy3.clj
+++ foo/src/foo/andy3.clj
@@ -4,4 +4,18 @@
 
 (addOne 1 2 3)
 
+(conj '(1 2 3) 10 20 )
+
+;(conj #(1 2 3) 10)
+
+(def x #{1 2 3} )
+
+(if (x 20) (println "Woot") (println "Bogus") )
+
+(case 
+    (+ 30 2 )
+  20 ( + 3 2 )
+  15 ( * 3 5 )
+  32 ( + 1 1 )
+)
 
--- foo/src/foo/core.clj
+++ foo/src/foo/core.clj
@@ -1,25 +1,20 @@
 (ns foo.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn square [x] [x (* x x ) ] )
+(def  a (agent {} ))
+
+(send-off a assoc "foo" "bar")
+(send-off a assoc "goo" "gar")
+(send-off a assoc "twat" "monkey")
 
 (take 20 (repeat 10) )
-
 (take 20 (cycle '[1 2 3]))
 
-(defn foobar [x] (println x))
-
-(defn foobaz [x] (println x)) 
+(defn foobar [x]   (println x))
+(defn foobaz [x]   (println x)) 
 (defn raporbaz [x] (println x))
 
-; (spit "/home/andy/foo.txt" "bla\n")
-
 (def lines (slurp "/home/andy/foo.txt"))
-
 (def myLines 
   (let [ splitCsv #(seq (.split %1 ",") ) ]
     (map splitCsv ( .split lines "\n") )
@@ -30,22 +25,21 @@
   (fn [x] [(reduce + (range 1 x)) 
            (reduce * (range 1 x) ) ] ) )
 
-(def addOne #(+ %1 1 ))
+(def addOne #(+ %1 1 ) )
 
-(range 1 10 20)
+(def addOne (partial + 1 ))
 
-; (defn nz [x] ( (comp not zero?) x ))
+(range 1 100 2 )
 
-(def nz (comp not zero?))
+(def non-zero? (comp not zero?))
 
-(letfn [ (nz [x] ( (comp not zero?) x )) ]
-  ( ->> 
-    (range)
-    (take 1000) 
-    (filter #( nz (mod %1 2 )) ) 
-    (filter #( nz (mod %1 4 )) ) 
-    (filter #( nz (mod %1 6 )) ) 
-    )
+( ->> 
+  (range)
+  (take 30) 
+  (filter #( non-zero? (mod %1 2 )) ) 
+  (filter #( non-zero? (mod %1 3 )) ) 
+  (filter #( non-zero? (mod %1 5 )) ) 
+  (filter #( non-zero? (mod %1 7 )) ) 
   )
 
 (def foo { :city { :burough { :name "vauxhall" }}})
@@ -54,33 +48,49 @@
 
 (defn prune-zeros
   "Remove zeros from dictionary" 
-  [ x ] (into {} (for [ [ q v ] (seq x) :when (not (zero? v) ) ] [q v] ) ) )
+  [ x ] (into {} (for [ [ q v ] (seq x) :when (non-zero? v) ] [q v] ) ) )
+
+(defn prune-zeros
+  "Remove zeros from dictionary" 
+  [ x ] (into {} ( for [ [ q v ] x :when (non-zero? v) ] [q v] ) ) )
+
 
 (def portOne { "VOD.L" 100
                "GLW.L" 500
+               "BASFn.DE" 30
                "RR.L"  200 })
 
 (def portTwo { "BASFn.DE" -100
                "GLW.L"    -500
                "RR.L"     -40 })
 
-(def portThree { "AAPL.O" -100 })
-               
-(def port4 (prune-zeros (merge-with + portOne portTwo portThree) ))
+(def portThree { 
+                "BASFn.DE" 101
+                "AAPL.O" -100 
+                } )
+
+(defn square [x] ( * x x ) )
+
+(square 3)
+
+;(merge-with + portOne portTwo portThree)
+(prune-zeros (merge-with + portOne portTwo portThree) )
 
 (defn makeSeq 
   "Construct an arbitrary sequence for the pythagorean triples exmaple" 
   [] (range 1 100))
 
 (letfn [ 
-        (makeSeq [] (range 100) ) 
+        (makeSeq [] (range 1 10) ) 
         (sq [x] ( * x x ))
         (isTriple [x y z] (= (+ (sq x) (sq y) ) (sq z)) )
         ]
   (for [ 
         x (makeSeq) 
-        y (makeSeq) 
-        z (makeSeq) 
+        y (range x 20)
+        z (range y 20)
         :when (isTriple x y z )
         ]  
-    [x y z] ))
+    [x y z]))
+
+(def a (agent {}))
