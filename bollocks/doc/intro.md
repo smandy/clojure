f748d0fcef16e6b69272ebba82f1d26b45f90023
Andy Smith <andyrsmith@googlemail.com>
2015-06-23 13:30:58 +0100

    Incrmental - SDL moved out into its own repo

--- bollocks/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to bollocks
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- bollocks/test/bollocks/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns bollocks.core-test
-  (:require [clojure.test :refer :all]
-            [bollocks.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
