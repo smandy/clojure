2963116aa6c09e62f7093f3a25844f4264cb5c50
Andy Smith <andyrsmith@gmail.com>
2015-06-23 13:30:58 +0100

    Incrmental - SDL moved out into its own repo

--- bollocks/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject bollocks "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[cider/cider-nrepl "0.8.2"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cider/cider-nrepl "0.8.2"]]
-)
